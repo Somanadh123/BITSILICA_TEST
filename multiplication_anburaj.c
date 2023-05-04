3f0a05aab50afcd982aed3060537160df71146dd
Mahavishnu <mahavishnu721@gmail.com>
2023-05-04 19:10:31 +0530

    update from maha

--- multiplication_anburaj.c
+++ multiplication_anburaj.c
@@ -1,3 +1,4 @@
+#include<stdio.h>
 void multiplication_anburaj(int a, int b)
 {
 	printf("%d\n",a*b);
--- vaibhav.c
+++ vaibhav.c
@@ -1,3 +1,4 @@
+#include<stdio.h>
 void addition_vaibhav(int a, int b)
 {
 printf("addition=%d\n",a+b);
