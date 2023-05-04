b5f974ea967d52b41377fab6f2b4bf5aae441973
upputuri somanadh <114662084+Somanadh123@users.noreply.github.com>
2023-05-04 16:04:53 +0530

    Create mycalc.h

---
+++ mycalc.h
@@ -0,0 +1,8 @@
+void addition_vaibhav(int a, int b);
+void substraction_murali(int a, int b);
+void multiplication_anburaj(int a, int b);
+void devision_mahima(int a, int b);
+void max_number_navya(int a, int b);
+void min_number_saravanan(int a, int b);
+void odd_or_even_mahavishnu(int n);
+void factors_ramanjaneyulu(int n);
