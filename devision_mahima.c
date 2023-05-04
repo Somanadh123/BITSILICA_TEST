0e09b2389d51609408c5bec3e9178015812d05e7
mahiima11 <ganjimahima@gmail.com>
2023-05-04 18:37:17 +0530

    division done

--- devision_mahima.c
+++ devision_mahima.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-void division_mahima(int a,int b)
+void devision_mahima(int a,int b)
 {
 	float c=a/b;
 	printf("the division value is %f",c);
---
+++ main.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include"mycalc.h"
+
+void main()
+{
+
+}
