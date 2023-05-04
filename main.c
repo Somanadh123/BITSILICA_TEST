60c38ed2c9ed8d5b5d0178fb1ffaef6af87308d8
saravanan6216 <saravanan6216@gmail.com>
2023-05-04 18:49:55 +0530

    final

---
+++ main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include"mycalc.h"
+void main()
+{
+	int a=5,b=10;
+     addition_vaibhav(a,b);
+     printf("\n");
+      max_number_navya(a, b);
+      printf("\n");
+      multiplication_anburaj(a,b);
+      printf("\n");
+      min_number_saravanan(a,b);
+      printf("\n");
+      odd_or_even_mahavishnu(a);
+      printf("\n");
+      devision_mahima(b,a);
+}
