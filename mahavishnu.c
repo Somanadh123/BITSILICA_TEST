53126f102f66b4b9b66fdfffeeb08020a3e301af
Mahavishnu <mahavishnu721@gmail.com>
2023-05-04 17:33:04 +0530

    mahavishnu code

--- mahavishnu.c
+++ mahavishnu.c
@@ -2,10 +2,10 @@ void odd_or_even_mahavishnu(int n)
 {
 	if(n%2==0)
 	{
-		printf("%d is even number\n");
+		printf("%d is even number\n",n);
 	}
 	else
 	{
-		printf("%d is odd number\n");
+		printf("%d is odd number\n",n);
 	}
 }
