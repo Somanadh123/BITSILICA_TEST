8214ad937585702b2e66fd215ab880eafe87b3b8
anbu4796 <anburajrko438@gmail.com>
2023-05-05 12:19:03 +0530

    branch files

---
+++ mainfiile.c
@@ -0,0 +1,13 @@
+#include"mycalc.h"
+#include<stdio.h>
+void main()
+{
+	addition_vaibhav(5,6);
+//void substraction_murali(5,6);
+ multiplication_anburaj(7,8);
+devision_mahima(6,7);
+ max_number_navya(2,3);
+min_number_saravanan(7,8);
+odd_or_even_mahavishnu(7);
+//factors_ramanjaneyulu(8);
+}
