ca3d7956bff8293759b504d9ed592905d728d9d7
mahiima11 <ganjimahima@gmail.com>
2023-05-05 09:44:12 +0530

    div done

--- devision_mahima.c
+++ devision_mahima.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 void devision_mahima(int a,int b)
 {
-	float c=a/b;
-	printf("the division value is %f",c);
+        int c=a/b;
+        printf("the division value is %d",c);
 }
--- main.c
+++ main.c
@@ -3,5 +3,11 @@
 
 void main()
 {
-
+	int a=10,b=5;
+	addition_vaibhav(a,b);
+        multiplication_anburaj(a,b);
+	devision_mahima(a,b);
+	max_number_navya(a,b);
+	min_number_saravanan(a,b);
+	odd_or_even_mahavishnu(a);
 }
