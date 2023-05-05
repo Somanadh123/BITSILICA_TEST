30726b747366fbc0e5f39701bf47e3b4f488740d
cchandramurali1 <cchandramurali@gmail.com>
2023-05-05 12:41:14 +0530

    murali

---
+++ main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include"mycalc.h"
+
+void main()
+{
+	int a=10,b=5;
+	addition_vaibhav(a,b);
+        multiplication_anburaj(a,b);
+	devision_mahima(a,b);
+	max_number_navya(a,b);
+	min_number_saravanan(a,b);
+	odd_or_even_mahavishnu(a);
+}
