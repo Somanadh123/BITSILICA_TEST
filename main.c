3ca65671ffda68bb5ce4f803bb9cec88344a004b
Mahavishnu <mahavishnu721@gmail.com>
2023-05-05 12:21:16 +0530

    updated

---
+++ main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include"mycalc.h"
+int main()
+{
+	int a=10,b=20;
+	//scanf("%d %d",&a,&b);
+	addition_vaibhav(a,b);
+	//substraction_murali(a,b);
+	multiplication_anburaj(a,b);
+	devision_mahima(a,b);
+	max_number_navya(a,b);
+	min_number_saravanan(a,b);
+	odd_or_even_mahavishnu(a);
+//	factors_ramanjaneyulu(a);
+	}
+
