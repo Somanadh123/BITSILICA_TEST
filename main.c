f5e384ef2842091a97045be20adc9e963e435f9c
navyavemula26 <vemulanavya3@gmail.com>
2023-05-05 12:05:32 +0530

    commit by navya

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "mycalc.h"
+void main()
+{
+	int a=10,b=12;
+	addition_vaibhav(10,12);
+	printf("\n");
+	max_number_navya(10,12);
+	multiplication_anburaj(10,12);
+	min_number_saravanan(10,12);
+	devision_mahima(10,12);
+	odd_or_even_mahavishnu(10);
+	substraction_murali(12,10);
+}
